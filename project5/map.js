9b8359a7d2e538151b5cae60aae5f9ecefce5c33
yvedeng <yvedeng@github.com>
2018-01-02 22:36:50 +0100

    map update

--- project5/map.js
+++ project5/map.js
@@ -1,5 +1,7 @@
 d3.select(window).on('load',init);
 
+console.log("test1");
+
 function init() {
     var svg  = d3.selectAll('svg');
     var margin = {top:50, right:50 , bottom : 200, left: 50 };
@@ -7,29 +9,41 @@ function init() {
     var height = +svg.node().getBoundingClientRect().height - margin.top - margin.bottom;
     var g = svg.append("g")
                 .attr("transform",
-                      "translate(" + margin.top + "," + margin.left + ")");
+                      "translate(" + 50 +"," + 50 + ")");
 
-    console.log("width", width);
-    console.log("height", height);
+    //choose projection
+    //var projection  = d3.geoMercator()
+                        //.scale(300000)
+                        //.rotate([122.419416, -37.774929])
+                        //.translate([width/6, height/6]);
+    var projection = d3.geoKavrayskiy7()
+        .scale(width*250)
+        .rotate([122.419416, -37.774929])
+        .translate([width / 2, height / 2]);
 
-    var projection  = d3.geoKavrayskiy7()
-        .scale(width/2)
-        .translate([width/2, height/2]);
+    //define path generator
 
     var path = d3.geoPath()
-        .projection(projection);
+                .projection(projection);
 
-    d3.json("sfpd_districts.geojson", function(error, data){
-        if (error) throw error;
-        g.append("g")
-            .attr("class", "districts")
-            .selectAll("path")
-            .data(data.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .style("fill","#ccc")
-            .style("stroke","#aaa");
-    })
+        d3.json("sfpd_districts.geojson", function(city){
+
+            console.log("boundary");
+            console.log(city.features);
+            //console.log(topojson.feature(city, city.objects.countries).features);
+
+
+            g.append("g")
+                .selectAll("path")
+                .data(city.features)
+                .enter()
+                .append("path")
+                .attr("d",path)
+                .style("fill","steelblue")
+                .style("stroke","black");
+
+            console.log("over");
+
+        });
 
 }
